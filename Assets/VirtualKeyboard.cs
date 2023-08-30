30c3411b15763ba196a2c25d499d385c0184a6df
mmanevski <markom@tabtale.com>
2023-08-30 16:45:43 +0200

    test

--- Assets/VirtualKeyboard.cs
+++ Assets/VirtualKeyboard.cs
@@ -7,6 +7,66 @@ public class VirtualKeyboard : MonoBehaviour {
 
     public MyInputField InputField;
 
+    private KeyCode keyPressed;
+    private bool isPressed;
+
+    private Coroutine processKeyCoro;
+
+    public void HandleKeyPressed(KeyCode key)
+    {
+
+        if (key.ToString().Length == 1 && char.IsLetter(key.ToString()[0]))
+        {
+            //This is your desired action
+            KeyPress(key.ToString());
+
+        }
+        if (key == KeyCode.LeftArrow)
+        {
+            KeyLeft();
+        }
+        if (key == KeyCode.RightArrow)
+        {
+            KeyRight();
+        }
+        if (key == KeyCode.Delete)
+        {
+            KeyDelete();
+        }
+    }
+
+    public void OnKeyPressed(int key)
+    {
+        keyPressed = KeyCode.A;
+        isPressed = true;
+        processKeyCoro = StartCoroutine(ProcessKey());
+    }
+
+    public void OnKeyReleased()
+    {
+        keyPressed = KeyCode.None;
+        isPressed = false;
+        StopCoroutine(processKeyCoro);
+    }
+
+    private IEnumerator ProcessKey()
+    {
+        WaitForSeconds _longPressTrigger = new WaitForSeconds(1f);
+        WaitForSeconds _longPressRepeat = new WaitForSeconds(0.1f);
+        
+        HandleKeyPressed(keyPressed);
+
+        yield return _longPressTrigger;
+   
+        while (isPressed)
+        {
+            HandleKeyPressed(keyPressed);
+            yield return _longPressRepeat;
+        }
+
+        yield break;
+    }
+
     public void KeyPress(string c)
     {
         InputField.AddCharacter(c);
